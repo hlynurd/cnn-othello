42602cf1dbcc78df156c7b958a5cb4dae6852c49
Hlynsson <hlynurd@gmail.com>
2016-10-24 21:22:14 +0200

    added 8 layer network

--- policy_networks.py
+++ policy_networks.py
@@ -212,3 +212,66 @@ def policy_net_7_layers(filters):
     
     return sess.graph, img_data, train_step, optimizer, labels, loss, predictions, keep_prob, learn_rate, score_out
     
+
+def policy_net_8_layers(filters):
+    img_data = tf.placeholder(tf.float32, shape=[None, 8, 8, 28], name="img_data")
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+    
+    #convolutional layers 
+    i = 28; o = filters;
+    conv1_weights = conv_weights(i, o)
+    conv1_biases = conv_biases(o)
+    conv1_out = conv_nonparams(img_data, conv1_weights, conv1_biases, keep_prob)
+    
+    i = o
+    conv2_weights = conv_weights(i, o)
+    conv2_biases = conv_biases(o)
+    conv2_out = conv_nonparams(conv1_out, conv2_weights, conv2_biases, keep_prob)
+    
+    conv3_weights = conv_weights(i, o)
+    conv3_biases = conv_biases(o)
+    conv3_out = conv_nonparams(conv2_out, conv3_weights, conv3_biases, keep_prob)
+    
+    conv4_weights = conv_weights(i, o)
+    conv4_biases = conv_biases(o)
+    conv4_out = conv_nonparams(conv3_out, conv4_weights, conv4_biases, keep_prob)
+    
+    conv5_weights = conv_weights(i, o)
+    conv5_biases = conv_biases(o)
+    conv5_out = conv_nonparams(conv4_out, conv5_weights, conv5_biases, keep_prob)
+    
+    conv6_weights = conv_weights(i, o)
+    conv6_biases = conv_biases(o)
+    conv6_out = conv_nonparams(conv5_out, conv6_weights, conv6_biases, keep_prob)
+    
+    conv7_weights = conv_weights(i, o)
+    conv7_biases = conv_biases(o)
+    conv7_out = conv_nonparams(conv6_out, conv7_weights, conv7_biases, keep_prob)
+    
+    conv8_weights = conv_weights(i, o)
+    conv8_biases = conv_biases(o)
+    conv8_out = conv_nonparams(conv7_out, conv8_weights, conv8_biases, keep_prob)
+    
+    #scoring layer
+    k = 1; i = o; o = 2;
+    score_weights = weight_variable([k, k, i, o])
+    score_biases = bias_variable([o])
+    score_out = conv_nonparams(conv8_out, score_weights, score_biases, keep_prob)
+
+
+    #final layer
+    predictions = softmax(score_out, o)
+
+    #training block:
+    labels = tf.placeholder(tf.int64, shape=[None, 8, 8, 1], name="ground_truths")
+    learn_rate = tf.placeholder(tf.float32, name="eta")
+    
+    
+    loss = semantic_loss(score_out, labels, o)    
+    optimizer = tf.train.AdamOptimizer(learn_rate)
+    train_step = optimizer.minimize(loss)
+    
+    sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    
+    return sess.graph, img_data, train_step, optimizer, labels, loss, predictions, keep_prob, learn_rate, score_out
+    
