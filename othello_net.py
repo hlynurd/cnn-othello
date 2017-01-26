ab5ced07e061600c4045adf418fa7361436e8184
Hlynsson <hlynurd@gmail.com>
2017-01-26 19:04:55 +0100

    stable SL

--- othello_net.py
+++ othello_net.py
@@ -1,5 +1,6 @@
 # CP HERE :
 import tensorflow as tf
+from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
 
 def conv_biases(o):
     shape = [o]
@@ -9,10 +10,19 @@ def conv_biases(o):
 def conv(input, kernel, p="SAME"):
     return tf.nn.conv2d(input, kernel, strides=[1, 1, 1, 1], padding=p)
 
-def conv_nonparams(tensor_input, conv_weights, conv_biases, keep_prob):
+def conv_nonparams(tensor_input, conv_weights, conv_biases, keep_prob, training, use_bn):
     conv_in = conv(tensor_input, conv_weights)
-    conv_relu = tf.nn.relu(conv_in + conv_biases)
+    if use_bn:
+        conv_bn = batch_norm_layer(conv_in + conv_biases, training)
+        conv_relu = tf.nn.relu(conv_bn)        
+    else:
+        conv_relu = tf.nn.relu(conv_in + conv_biases)
     conv_drop = tf.nn.dropout(conv_relu, keep_prob)
+    return conv_drop
+
+def final_conv_nonparams(tensor_input, conv_weights, conv_biases, keep_prob):
+    conv_in = conv(tensor_input, conv_weights)
+    conv_drop = tf.nn.dropout(conv_in + conv_biases, keep_prob)
     return conv_drop
 
 def weight_variable(shape):
@@ -41,25 +51,38 @@ def semantic_loss(logits, labels, o):
     loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits_reshaped, labels_reshaped)
     return loss
 
-def create_othello_net():
-    img_data = tf.placeholder(tf.float32, shape=[None, 8, 8, 28], name="img_data")
-    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+def batch_norm_layer(x,train_phase):
+    bn_train = batch_norm(x, decay=0.9, center=True, scale=False,
+    updates_collections=None,
+    is_training=True)
     
+    bn_inference = batch_norm(x, decay=0.9, center=True, scale=False,
+    updates_collections=None,
+    is_training=False)
+    z = tf.cond(train_phase, lambda: bn_train, lambda: bn_inference)
+    return z
+
+
+def create_othello_net():
+    use_batch_normalization = False
+    img_data = tf.placeholder(tf.float32, shape=[None, 8, 8, 43], name="img_data")
+    keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+    training = tf.placeholder(tf.bool, name="training")
     #convolutional layers 
-    i = 28; o = 32;
+    i = 43; o = 32;
     conv1_weights = conv_weights(i, o)
     conv1_biases = conv_biases(o)
-    conv1_out = conv_nonparams(img_data, conv1_weights, conv1_biases, keep_prob)
+    conv1_out = conv_nonparams(img_data, conv1_weights, conv1_biases, keep_prob, training, use_batch_normalization)
     
     i = o; o = 32;
     conv2_weights = conv_weights(i, o)
     conv2_biases = conv_biases(o)
-    conv2_out = conv_nonparams(conv1_out, conv2_weights, conv2_biases, keep_prob)
+    conv2_out = conv_nonparams(conv1_out, conv2_weights, conv2_biases, keep_prob, training, use_batch_normalization)
     
     i = o; o = 32;
     conv3_weights = conv_weights(i, o)
     conv3_biases = conv_biases(o)
-    conv3_out = conv_nonparams(conv2_out, conv3_weights, conv3_biases, keep_prob)
+    conv3_out = conv_nonparams(conv2_out, conv3_weights, conv3_biases, keep_prob, training, use_batch_normalization)
     
     #i = o; o = 32;
     #conv4_weights = conv_weights(i, o)
@@ -81,7 +104,7 @@ def create_othello_net():
     k = 1; i = o; o = 2;
     score_weights = weight_variable([k, k, i, o])
     score_biases = bias_variable([o])
-    score_out = conv_nonparams(conv3_out, score_weights, score_biases, keep_prob)
+    score_out = final_conv_nonparams(conv3_out, score_weights, score_biases, keep_prob)
 
 
     #final layer
@@ -98,5 +121,5 @@ def create_othello_net():
     
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
     
-    return sess.graph, img_data, train_step, optimizer, labels, loss, predictions, keep_prob, learn_rate, score_out
+    return sess.graph, img_data, train_step, optimizer, labels, loss, predictions, keep_prob, learn_rate, score_out, training
     
